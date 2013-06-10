379ca970ec6f407a92f103166b52a83d81b87617
drobinsonessa@gmail.com <drobinsonessa@gmail.com@90b89b48-9230-0de4-5065-812dd12741b8>
2013-06-10 18:54:32 +0000

    renaming "Testing" to "Example"

--- VB.Net-FVS/Testing/My Project/Application.Designer.vb
+++ VB.Net-FVS/Testing/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Testing.Testing
+         Me.MainForm = Global.Testing.Example
         End Sub
     End Class
 End Namespace
